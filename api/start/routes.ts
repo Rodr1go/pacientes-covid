b00824db7c5ec9cea60784bd610bd36c57726c25
Rodrigo Santos <rodrigosc.pi@gmail.com>
2021-07-19 17:41:04 -0300

    Routes for handling the patient controller

--- api/start/routes.ts
+++ api/start/routes.ts
@@ -20,6 +20,6 @@
 
 import Route from '@ioc:Adonis/Core/Route'
 
-Route.get('/', async () => {
-  return { hello: 'world' }
-})
+Route.group(() => {
+  Route.resource('pacientes', 'PacientesController').apiOnly();
+}).prefix('api/v1');
